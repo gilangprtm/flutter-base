129d49ab30aef5dab04fa9c82d2cfe6507a1155c
gilang <gilangprtm210498@gmail.com>
2023-04-04 16:35:34 +0800

    TF-70 #perbaikan auto refresh dan type field nrm

--- lib/app/modules/dokter_konfirmasi_tab/controllers/dokter_konfirmasi_tab_controller.dart
+++ lib/app/modules/dokter_konfirmasi_tab/controllers/dokter_konfirmasi_tab_controller.dart
@@ -16,8 +16,7 @@ import '../../../mahas/services/http_api.dart';
 class DokterKonfirmasiTabController extends GetxController {
   final namaPasienCon = InputDropdownController();
   late InputRadioController tipePasienCon;
-  final InputTextController nrmCon =
-      InputTextController(type: InputTextType.number);
+  final InputTextController nrmCon = InputTextController();
   final InputTextController noHPCon =
       InputTextController(type: InputTextType.number);
 
@@ -63,7 +62,10 @@ class DokterKonfirmasiTabController extends GetxController {
   }
 
   void goToTambahPasien() {
-    Get.toNamed(Routes.PASIEN_SETUP, parameters: {"getData": true.toString()});
+    Get.toNamed(Routes.PASIEN_SETUP, parameters: {"getData": true.toString()})!
+        .then((value) => {
+              getDataPasien(),
+            });
   }
 
   Future<JadwalpraktekModel> getDataDokter() async {
