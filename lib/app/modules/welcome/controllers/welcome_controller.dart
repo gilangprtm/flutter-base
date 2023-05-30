30619ee3d1cc51d9fd55018031e0606249c97627
Freddy Artha <freddyartha@gmail.com>
2023-05-30 11:32:24 +0800

    TF-479 #[Haimed] User Bisa Melihat Welcome Screen saat pertama kali instal apps

--- lib/app/modules/welcome/controllers/welcome_controller.dart
+++ lib/app/modules/welcome/controllers/welcome_controller.dart
@@ -1,13 +1,12 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import '../../../routes/app_pages.dart';
 
 class WelcomeController extends GetxController {
-  void onStarted() {
-    Get.offAllNamed(Routes.home);
-  }
-
-  void onSkip() {
-    Get.toNamed(Routes.LOGIN);
+  final box = GetStorage();
+  void onStarted() async {
+    await box.write("new_install", false);
+    Get.offAllNamed(Routes.LOGIN);
   }
 }
