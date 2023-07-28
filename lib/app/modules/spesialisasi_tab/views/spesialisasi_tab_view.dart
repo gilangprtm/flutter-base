f30e537d367c874d5aa15a57b9cac9d80f50d1fa
Freddy Artha <freddyartha@gmail.com>
2023-07-28 11:14:52 +0800

    TF-663 #[Haimed] Add Icon Spesialisasi in List Spesialisasi Tab HaiMed Premagana

--- lib/app/modules/spesialisasi_tab/views/spesialisasi_tab_view.dart
+++ lib/app/modules/spesialisasi_tab/views/spesialisasi_tab_view.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 import 'package:haimed_getx/app/mahas/components/mahas_themes.dart';
 import 'package:haimed_getx/app/mahas/components/others/list_component.dart';
 
-import '../../../mahas/mahas_colors.dart';
 import '../../../models/spesialisasi_model.dart';
 import '../controllers/spesialisasi_tab_controller.dart';
 
@@ -21,12 +20,20 @@ class SpesialisasiTabView extends GetView<SpesialisasiTabController> {
             contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
             onTap: () =>
                 controller.goToDetailPage(e.spesialisid!, e.namaspesialisasi!),
-            leading: Icon(
-              Icons.health_and_safety,
-              color: MahasColors.red,
-              size: 30,
+            leading: ClipOval(
+              child: e.spesialisasiURL != null
+                  ? Image.network(
+                      e.spesialisasiURL!,
+                      width: 50,
+                      height: 50,
+                    )
+                  : Image.asset(
+                      "assets/images/ic_poli_default.PNG",
+                      width: 50,
+                      height: 50,
+                    ),
             ),
-            horizontalTitleGap: 0,
+            horizontalTitleGap: 15,
             title: Text(
               e.namaspesialisasi ?? "",
               style: MahasThemes.h3,
